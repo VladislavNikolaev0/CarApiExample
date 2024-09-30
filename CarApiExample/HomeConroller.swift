fbb391a2a65d94a1b44dde36dfa78ed70d99ab7a
VladislavNikolaev0 <mr.nikolaev.vladislav.valen@mail.ru>
2024-09-30 21:01:00 +0300

    add collection view flow layout

--- CarApiExample/HomeConroller.swift
+++ CarApiExample/HomeConroller.swift
@@ -9,7 +9,8 @@ import UIKit
 
 final class HomeConroller: UIViewController {
     
-    var cars: [Cars] = []
+    private var cars: [Cars] = []
+    private lazy var collectionVeiw = UICollectionView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,5 +25,32 @@ final class HomeConroller: UIViewController {
             print(cars.count)
             print(cars)
         }
+        configureCollectionView()
+    }
+    
+    private func configureCollectionView() {
+        collectionVeiw = UICollectionView(frame: view.bounds, collectionViewLayout: getCollectionFlowLayout())
+        view.addSubview(collectionVeiw)
+        collectionVeiw.backgroundColor = .cyan
+        collectionVeiw.register(CarCell.self, forCellWithReuseIdentifier: "carID")
+    }
+    
+    private func getCollectionFlowLayout() -> UICollectionViewFlowLayout {
+        let width = view.bounds.width
+        let padding: CGFloat = 12
+        let minimumItemSpacing: CGFloat = 10
+        let avialabelWidth = width - (padding * 2) - minimumItemSpacing
+        let itemWidth = avialabelWidth / 2
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(
+            top: padding,
+            left: padding,
+            bottom: padding,
+            right: padding
+        )
+        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
+        
+        return flowLayout
     }
 }
